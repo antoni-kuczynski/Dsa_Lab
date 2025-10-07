e58fdde5bb8f49839bc9f86d4fbfee570705eb98
Antoni Kuczyński <antoni.kucz@gmail.com>
2025-10-08 00:39:48 +0200

    Added HotOrColdProblem

--- src/main/java/pl/edu/pw/ee/aisd_ex_0/extratasks/HotOrColdProblem.java
+++ src/main/java/pl/edu/pw/ee/aisd_ex_0/extratasks/HotOrColdProblem.java
@@ -8,7 +8,34 @@ Wywołujesz wcześniej zdefiniowane API int guess(int num).
 
 public class HotOrColdProblem {
 
+    private int guess(int num) {
+        return num % 3 - 1;
+    }
+
     public int guessNumber(int n) {
-    	throw new UnsupportedOperationException("TODO hot or cold problem");
+        validateN(n);
+
+    	int left = 0;
+        int right = n;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            int guess = guess(middle);
+
+            if (guess == 1) {
+                right = middle - 1;
+            } else if (guess == -1) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+
+    private void validateN(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n cannot be negative");
+        }
     }
 }
