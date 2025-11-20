02f04f7c7f1f15340c171bad3b5200cff51d0e43
Antoni Kuczyński <antoni.kucz@gmail.com>
2025-11-20 20:53:52 +0100

    a

---
+++ cw4/AiSD2025ZEx4/src/test/java/pl/edu/pw/ee/aisd2025zex4/RbtMapTest.java
@@ -0,0 +1,35 @@
+package pl.edu.pw.ee.aisd2025zex4;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.edu.pw.ee.aisd2025zex4.service.MapInterface;
+
+public class RbtMapTest {
+
+    private MapInterface<Integer, String> students;
+
+    @BeforeEach
+    public void setup() {
+        students = new RbtMap<>();
+    }
+
+    @Test
+    public void should_ThrowException_When_PuttingNullKey() {
+        // given
+        Integer studentId = null;
+        String studentFullName = "Miś Uszatek";
+
+        // when
+        Throwable thrown = catchThrowable(() -> {
+            students.setValue(studentId, studentFullName);
+        });
+
+        // then
+        assertThat(thrown)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Params (key, value) cannot be null.");
+    }
+
+}
