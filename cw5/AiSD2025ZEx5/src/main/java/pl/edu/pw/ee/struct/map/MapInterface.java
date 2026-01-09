b3a7377726f8dd7cef1c9ecd44ee7e49eba10da1
Antoni Kuczyński <antoni.kucz@gmail.com>
2026-01-09 23:50:22 +0100

    cw2

--- cw5/AiSD2025ZEx5/src/main/java/pl/edu/pw/ee/struct/map/MapInterface.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.pw.ee.struct.map;
-
-public interface MapInterface<V> {
-
-    public void put(byte[] key, V value);
-
-    public V get(byte[] key);
-}
--- cw6/AiSD2025ZEx6/src/test/java/pl/edu/pw/ee/aisd2025zex6/matrixchainorder/MatrixChainOrderTopDownTest.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.pw.ee.aisd2025zex6.matrixchainorder;
-
-public class MatrixChainOrderTopDownTest extends MatrixChainOrderTest {
-    public MatrixChainOrderTopDownTest() {
-        super(new MatrixChainOrderTopDown());
-    }
-}
