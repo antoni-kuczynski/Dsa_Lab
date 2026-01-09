6d243972bd6d2e18cc11396e120f48661a750ef9
Antoni Kuczyński <antoni.kucz@gmail.com>
2026-01-10 00:03:44 +0100

    cw7 start

--- BONUS_2026/AiSD2025ZBonus2026/src/main/java/pl/edu/pw/ee/aisd2025zbonus2026/christmaschain/ChristmasChain.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.pw.ee.aisd2025zbonus2026.christmaschain;
-
-public class ChristmasChain {
-
-    public int findLengthOfSubset(String[] chains, int nz, int nc) {
-        throw new UnsupportedOperationException("This task is for you! (GR 3)");
-    }
-
-}
--- cw6/AiSD2025ZEx6/src/test/java/pl/edu/pw/ee/aisd2025zex6/rodcuttingproblem/RodCutterBottomUpTest.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.pw.ee.aisd2025zex6.rodcuttingproblem;
-
-public class RodCutterBottomUpTest extends RodCutterTest {
-
-    public RodCutterBottomUpTest() {
-        super(new RodCutterBottomUp());
-    }
-}
