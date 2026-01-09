8ddbe1d6c5e29618b7baf1e32acdf6a4525f772e
Antoni Kuczyński <antoni.kucz@gmail.com>
2026-01-09 23:57:39 +0100

    cw5

--- cw6/AiSD2025ZEx6/src/test/java/pl/edu/pw/ee/aisd2025zex6/rodcuttingproblem/RodCutterRecursiveTest.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.pw.ee.aisd2025zex6.rodcuttingproblem;
-
-public class RodCutterRecursiveTest extends RodCutterTest {
-
-    public RodCutterRecursiveTest() {
-        super(new RodCutterRecursive());
-    }
-}
