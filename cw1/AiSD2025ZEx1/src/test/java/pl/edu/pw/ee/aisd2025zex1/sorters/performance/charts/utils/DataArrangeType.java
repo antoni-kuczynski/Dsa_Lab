a9a0380a1177ad23a98e5dbe5fc277d30dbe7452
Antoni Kuczyński <antoni.kucz@gmail.com>
2026-01-30 20:17:36 +0100

    repo extermination

--- cw1/AiSD2025ZEx1/src/test/java/pl/edu/pw/ee/aisd2025zex1/sorters/performance/charts/utils/DataArrangeType.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.pw.ee.aisd2025zex1.sorters.performance.charts.utils;
-
-public enum DataArrangeType {
-    ASC,
-    RAND,
-    DESC;
-}
--- cw6/AiSD2025ZEx6/src/test/java/pl/edu/pw/ee/aisd2025zex6/rodcuttingproblem/RodCutterTopDownTest.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.pw.ee.aisd2025zex6.rodcuttingproblem;
-
-public class RodCutterTopDownTest extends RodCutterTest {
-
-    public RodCutterTopDownTest() {
-        super(new RodCutterTopDown());
-    }
-}
